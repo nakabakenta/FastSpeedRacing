76872915b10aeddb9c0b469dca3aa8ebbb69d5a3
DESKTOP-U0TU0SG\GA2B <h1202315003@hiratagakuen.onmicrosoft.com>
2024-07-10 12:08:33 +0900

    【2024/07/10】

---
+++ Assets/Scripts/UI_Scripts/UI_Text.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Text : MonoBehaviour
+{
+    private TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.gameObject.GetComponent<TMP_Text>();
+        text.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+        StartCoroutine("Flashing");
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                text.color = text.color - new Color32(0, 0, 0, 10);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            for (int k = 0; k < 25; k++)
+            {
+                text.color = text.color + new Color32(0, 0, 0, 10);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
